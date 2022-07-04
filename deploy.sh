f0c5cdf451fa4c5377dd0e6976dfd8564c578202
Deven <kwongtakwai98@gmail.com>
2022-07-04 20:25:45 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+yarn run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'deven.tw' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:deventw/my-homepage.git master:gh-pages
+cd -
