c7498166c7e7bfd38177b23648461b3aedc6803a
Deven <kwongtakwai98@gmail.com>
2022-10-23 00:53:59 +0800

    update spin card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,16 +3,12 @@ import React from 'react'
 export const Main = () => {
   return (
     <div>
-        <p>Heyo! I'm DevenTW.</p>
+        <p>Heyo! I&apos;m DevenTW.</p>
         {/* <p>I live in Hong Kong and just graduated 🎓.</p> */}
         <p>I live in Hong Kong and work as a Developer 👨‍💻. </p>
 
-        <p> I am also a
-            <a href="https://blog.deven.tw">Blogger</a>📝,
-            <a href="https://instagram.com/wt.neved">Photographer</a>📸.
-
-            {/* <a href="https://pho.deven.tw">Photographer</a> 📸.
-            and <a href="https://blog.deven.tw/travel/">Traveler</a> 🌍.  */}
+        <p> I am also a <a href="https://blog.deven.tw">Blogger</a>📝, <a href="https://instagram.com/wt.neved">Photographer</a>📸.
+            {/* <a href="https://pho.deven.tw">Photographer</a> 📸. and <a href="https://blog.deven.tw/travel/">Traveler</a> 🌍.  */}
         </p>
             {/* <p>
                     <a href="/lib.html">Here</a> are some projects I have made.
