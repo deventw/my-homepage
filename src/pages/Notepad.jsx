e27b15dea0ba2f9d539f6a5731d26567dbcc15c9
Deven <kwongtakwai98@gmail.com>
2022-11-05 16:02:29 +0800

    fix redirect issues

--- src/pages/Notepad.jsx
+++ src/pages/Notepad.jsx
@@ -4,11 +4,11 @@ import { Note } from '../components/Note';
 
 export const Notepad = ({notes,addNote,removeNote}) => {
 
-    return (
-        <div className="w-full">
-            <AddNote addNote={addNote}/>
-            {notes.map((note) =>(<Note key={note.id} id={note.id} text={note.text} date={note.date} removeNote={removeNote}  />))}
+	return (
+		<div className="w-full">
+			<AddNote addNote={addNote}/>
+			{notes.map((note) =>(<Note key={note.id} id={note.id} text={note.text} date={note.date} removeNote={removeNote}  />))}
 			
-        </div>
-    );
+		</div>
+	);
 };
