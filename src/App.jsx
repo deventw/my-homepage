f7fe79a080cf2a20fab08cee8d5a57a16197347e
Deven <kwongtakwai98@gmail.com>
2022-11-05 15:51:48 +0800

    added notepad

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import { nanoid } from 'nanoid';
+import React, { useEffect, useState } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { About } from './pages/About';
 import Home from './pages/Home';
+import { Notepad } from './pages/Notepad';
 
 function App() {
+	const [notes, setNotes] = useState([]);
+
+	useEffect(() =>{
+		const savedNotes = JSON.parse(localStorage.getItem('react-notes-app-data'));
+		if (savedNotes) {
+			setNotes(savedNotes);	
+		}
+	},[]);
+	useEffect(() =>{localStorage.setItem('react-notes-app-data',JSON.stringify(notes));},[notes]);
+
+	const addNote = (text) => {
+		const newNote = {
+			id:nanoid(),
+			text:text,
+			date:new Date().toLocaleString()
+		}; 
+		setNotes([...notes,newNote]);
+	};
+	const removeNote = (id) => {
+		setNotes(notes.filter((note)=>note.id !== id));
+	};
+
 	return (
 		<>
 			<nav>
-				<ul className="flex-row flex gap-4 m-4 justify-center">
+				<ul className="flex-row flex gap-4 m-8 justify-center">
 					<li><Link to="/">Home</Link></li>
+					<li><Link to="/notepad">Notepad</Link></li>
 					<li><Link to="/about">About</Link></li>
 				</ul>
 
@@ -17,14 +42,13 @@ function App() {
 			<></>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/notepad" element={<Notepad notes={notes} addNote={addNote} removeNote={removeNote}/>} />
 				<Route path="/about" element={<About />} />
 			</Routes>
 		</>
-
 
 	);
 }
 
 export default App;
-
 
---
+++ src/pages/Notepad.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AddNote from '../components/AddNote';
+import { Note } from '../components/Note';
+
+export const Notepad = ({notes,addNote,removeNote}) => {
+
+    return (
+        <div className="w-full">
+            <AddNote addNote={addNote}/>
+            {notes.map((note) =>(<Note key={note.id} id={note.id} text={note.text} date={note.date} removeNote={removeNote}  />))}
+			
+        </div>
+    );
+};
